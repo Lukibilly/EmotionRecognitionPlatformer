fb44b51d1e948f8f472860f17f208e756014c4d1
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-04 17:43:55 +0200

    Scene change and CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    [Range(0,20)]
+    public float smoothFactor;
+
+    private void FixedUpdate(){
+        Follow();
+    }
+    void Follow(){
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
