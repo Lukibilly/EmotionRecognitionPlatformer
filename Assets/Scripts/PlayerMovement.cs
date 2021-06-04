ded5c03a8c496155f34bc29f8157f511acb15253
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-04 16:32:53 +0200

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float runSpeed = 40f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float jumpPower = 300;
+
+    float groundCheckRadius = 0.2f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool facingright = true;
+    bool isGrounded = false;
+
+    void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButton("Jump")){
+            jump = true;
+        }
+    }
+
+    void FixedUpdate(){
+        Move(horizontalMove);
+        GroundCheck();
+    }
+
+    void GroundCheck(){
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position,groundCheckRadius,groundLayer);
+        if(colliders.Length>0)
+            isGrounded = true;
+    }
+    void Move(float dir){
+        if(isGrounded && jump){
+            isGrounded = false;
+            jump = false;
+            rb.AddForce(new Vector2(0f,jumpPower));            
+        }
+        #region MoveLeftRight
+        float xvelocity = dir*runSpeed*Time.fixedDeltaTime;
+        Vector2 targetVelocity = new Vector2(xvelocity,rb.velocity.y);
+        rb.velocity = targetVelocity;
+
+        if(facingright && dir<0){
+            transform.localScale = new Vector3(-1,1,1);
+            facingright = false;
+
+        }else if(!facingright && dir>0){
+            transform.localScale = new Vector3(1,1,1);
+            facingright = true;
+        }
+        animator.SetFloat("xVelocity",Mathf.Abs(rb.velocity.x));
+        #endregion
+    }
+}
