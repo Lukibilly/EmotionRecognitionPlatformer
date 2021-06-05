8baa5def39b9366997a601d1fe613bec4626a612
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-05 16:48:13 +0200

    Added LifeCount with UI access

---
+++ Assets/Scripts/LifeCount.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCount : MonoBehaviour
+{
+    public Image[] lives;
+    public int livesRemaining;
+
+    public void LoseLife(){
+        livesRemaining--;
+        lives[livesRemaining].enabled = false;
+    }
+}
