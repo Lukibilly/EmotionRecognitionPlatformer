2f6d73702aa1989621068b1c4b26244fefd79b42
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-06 14:49:52 +0200

    Player Color changes when damaged

--- Assets/Scripts/LifeCount.cs
+++ Assets/Scripts/LifeCount.cs
@@ -7,9 +7,22 @@ public class LifeCount : MonoBehaviour
 {
     public Image[] lives;
     public int livesRemaining;
+    public Color damageColor;    
+    public float damageBlinkTime;
+    public SpriteRenderer spriteRenderer;
+
+    float lastDamageTime = -100;
+    
+
+    void FixedUpdate(){
+        if((Time.time - lastDamageTime)>damageBlinkTime){
+            spriteRenderer.color = new Color(255,255,255);
+        }
+    }
 
     public void LoseLife(){
-
+        lastDamageTime = Time.time;
+        spriteRenderer.color = damageColor;
         if(livesRemaining>0){
             livesRemaining--;
             lives[livesRemaining].enabled = false;
