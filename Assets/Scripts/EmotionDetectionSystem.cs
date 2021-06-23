d9a256ca698a8d2625f73c701f8bb5231e4010c0
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-23 20:39:51 +0200

    Link Python fer with game

---
+++ Assets/Scripts/EmotionDetectionSystem.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionDetectionSystem : MonoBehaviour
+{
+    Process fer;
+    void Start()
+    {
+        startFer();
+    }
+    void OnApplicationQuit(){
+        fer.Kill();
+    }
+
+    void startFer(){
+        string ferpath = "C:\\Users\\631lh\\Documents\\UNI\\Neuro-Usability\\Facial-emotion-recognition\\dist\\live_cam_predict\\live_cam_predict.exe";
+        ProcessStartInfo ferInfo = new ProcessStartInfo();
+        ferInfo.CreateNoWindow = true;
+        ferInfo.UseShellExecute = false;
+        ferInfo.FileName = ferpath;
+        ferInfo.WorkingDirectory = "C:\\Users\\631lh\\Documents\\UNI\\Neuro-Usability\\Facial-emotion-recognition\\dist\\live_cam_predict";
+        fer = Process.Start(ferInfo);
+    }
+    
+}
