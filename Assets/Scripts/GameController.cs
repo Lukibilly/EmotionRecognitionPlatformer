68e6cca69981177ad97d7da28e638e821bef8b15
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-07-04 22:36:25 +0200

    Added a LoadinScreen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public static GameController instance;
+    public static GameController instance;    
     void Awake(){instance = this;}
 
     public bool gameStarted = false;
+    public GameObject loadingPanel;
+    public TextMeshProUGUI  loadingcontinue;
+    public Button continuebutton;
+    private bool clickedContinue = false;
+    private bool loadedModel = false;
     
     public void startGame(){
-        gameStarted = true;
+        loadedModel = true;
+        loadingcontinue.GetComponent<TextMeshProUGUI>().SetText("CONTINUE");
+        continuebutton.gameObject.SetActive(true);
+    }
+    public void clickContinue(){
+        clickedContinue = true;
+        if(loadedModel){
+            loadingPanel.SetActive(false);
+            gameStarted = true;
+        }
     }
 }
